f56a0e6760fe6f0efb8b0eb1d434fb84e183b858
root <root@DESKTOP-KMFLIKL.localdomain>
2022-11-10 12:40:27 +0800

    style: collapsed FAQS semi bold text

--- src/views/Accordion.js
+++ src/views/Accordion.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
 
+// helper functions to bold text specific part 
+// of the sentence
+function boldString(str, find){ 
+  let re = new RegExp(find, 'g'); 
+  return str.replace(re, '<b>'+find+'</b>'); 
+}
+
 const Accordion = ({
   title,
   step1,
@@ -36,6 +43,56 @@ const Accordion = ({
 }) => {
   const [isActive, setIsActive] = useState(false);
 
+  // extract text to add style 
+  let boldTextStep1 = ""; 
+  let boldTextStep2 = ""; 
+  let boldTextStep4 = ""; 
+  let boldTextStep5 = ""; 
+
+  const highlightText = { 
+    step1: ["Connect Wallet", "connect wallet"],
+    step2: ["My Collection", "Claim Qube", "5 Qubes"],
+    step4: ["Buy Now", "Download .glb", ],
+    step5: [".glb", "Submit"]
+  };
+
+  
+  // search string to bold text
+  if (step1){
+    highlightText.step1.map(targetText => { 
+      if (step1.includes(targetText)){
+        boldTextStep1 = boldString(step1, targetText);
+      } 
+    })
+    
+  } 
+  
+  if(step2){
+    highlightText.step2.map(targetText => {
+      if(step2.includes(targetText)){
+        boldTextStep2 = boldString(step2, targetText);
+      }
+    })
+  }
+
+  if(step4){
+    highlightText.step4.map(targetText => { 
+      if(step4.includes(targetText)){
+        boldTextStep4 = boldString(step4, targetText);
+      }
+    })
+  }
+
+  if(step5){ 
+    highlightText.step5.map(targetText => { 
+      if(step5.includes(targetText)){
+        boldTextStep5 = boldString(step5, targetText);
+      }
+    })
+  }
+
+  console.log(step2);
+
   return (
     <div className="accordion-item">
       <div
@@ -55,18 +112,18 @@ const Accordion = ({
 
       {isActive && (
         <div
-          className="accordion-content carbon-text"
-          style={{ fontSize: "1.25rem" }}
+          class="accordion-content carbon-text"
+          style={{ fontSize: "1.25rem", fontWeight: 600 }}
         >
-          <div class={state1}>
-            {step1}
-            <span class={statelink}>
-              {" "}
+          <div className={state1} >
+            {boldTextStep1 === "" ? step1 : <span dangerouslySetInnerHTML={{ __html: boldTextStep1}}></span>}
+            
+            <span className={statelink}>
               &nbsp;
               <a
                 href={steplink} // steplink
                 target="_blank"
-                class="blog-link blog-link-underline"
+                className="blog-link blog-link-underline fw-bold"
               >
                 {linkname}
               </a>
@@ -74,16 +131,16 @@ const Accordion = ({
             </span>
             <br /> <br />
           </div>
-          <div class={state2}>
-            {" "}
-            {step2}
-            <span class={statelink3}>
-              {" "}
+
+          <div className={state2}>
+            {boldTextStep2 === "" ? step2 : <span dangerouslySetInnerHTML={{ __html: boldTextStep2}}></span>}
+
+            <span className={statelink3}>
               &nbsp;
               <a
                 href={steplink1} // steplink
                 target="_blank"
-                class="blog-link blog-link-underline"
+                className="blog-link blog-link-underline fw-bold"
               >
                 {linkname1}
               </a>
@@ -91,16 +148,15 @@ const Accordion = ({
             </span>
             <br /> <br />
           </div>
-          <div class={state3}>
-            {" "}
+          
+          <div className={state3}>
             {step3}
-            <span class={statelink3}>
-              {" "}
+            <span className={statelink3}>
               &nbsp;
               <a
                 href={steplink2} // steplink
                 target="_blank"
-                class="blog-link blog-link-underline"
+                className="blog-link blog-link-underline fw-bold"
               >
                 {linkname2}
               </a>
@@ -108,29 +164,32 @@ const Accordion = ({
             </span>
             <br /> <br />
           </div>
-          <div class={state4}>
-            {" "}
-            {step4}
+
+          <div className={state4}>
+            {boldTextStep4 === "" ? step4 : <span dangerouslySetInnerHTML={{ __html: boldTextStep4}}></span>}
+
             <br /> <br />
           </div>
-          <div class={state5}>
-            {step5}
+
+          <div className={state5}>
+            {boldTextStep5 === "" ? step5 : <span dangerouslySetInnerHTML={{ __html: boldTextStep5}}></span>}
+
             <br /> <br />
           </div>
-          <div class={state6}>
+
+          <div className={state6}>
             {step6}
             <br /> <br />
           </div>
-          <div class={state7}>
-            {" "}
+
+          <div className={state7}>
             {step7}
-            <span class={statelink6}>
-              {" "}
+            <span className={statelink6}>
               &nbsp;
               <a
                 href={steplink3} // steplink
                 target="_blank"
-                class="blog-link blog-link-underline"
+                className="blog-link blog-link-underline fw-bold"
               >
                 {linkname3}
               </a>
@@ -138,7 +197,8 @@ const Accordion = ({
             </span>
             <br /> <br />
           </div>
-          <div class={state8}>{step8}</div>
+
+          <div className={state8}>{step8}</div>
           <hr />
         </div>
       )}
@@ -151,3 +211,6 @@ export default Accordion;
 // <a href="mailto:manufacturing@homeqube.com" target="_blank">
 //manufacturing@homeqube.com.
 //</a>
+
+
+
